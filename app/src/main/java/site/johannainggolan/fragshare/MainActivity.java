e24261618c95e3180ebf72dea9911d8655638012
johan <johan@bareksa.com>
2019-12-29 11:49:49 +0700

    adding construct

--- app/src/main/java/site/johannainggolan/fragshare/MainActivity.java
+++ app/src/main/java/site/johannainggolan/fragshare/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /*
+    *FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.layout_main, fragment)
+                        .commit();
+    *
+    * */
 }
